f76a8337e1d295dd18cadd721ad17d67e4002c4a
unknown <4purposeliving@gmail.com>
2018-07-16 11:13:09 -0700

    commit to save

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -38,25 +38,25 @@ class Login extends React.Component {
   render() {
     const { onRouteChange } = this.props;
     return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
-          <div className="measure">
-            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Employee Portal</legend>
-              <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+      <article className="">
+        <main className="">
+          <div className="">
+            <fieldset id="" className="">
+              <legend className="">Employee Portal</legend>
+              <div className="">
+                <label className="">Email</label>
                 <input
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className=""
                   type="email"
                   name="email-address"
                   id="email-address"
                   onChange={this.onEmailChange}
                 />
               </div>
-              <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+              <div className="">
+                <label className="" htmlFor="password">Password</label>
                 <input
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  className=""
                   type="password"
                   name="password"
                   id="password"
@@ -67,7 +67,7 @@ class Login extends React.Component {
             <div className="">
               <input
                 onClick={this.onSubmitLogin}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                className=""
                 type="submit"
                 value="Log in"
               />
